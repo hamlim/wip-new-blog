e9c496d601b5c62dbf50b21f9a11051b0821f61d
Matt Hamlin <matthewjameshamlin@gmail.com>
2025-04-23 19:23:48 -0400

    Image gen works - emoji reference is less than ideal IMO

--- scripts/og-image-gen.tsx
+++ scripts/og-image-gen.tsx
@@ -1,43 +1,58 @@
 /// <reference types="bun" />
-// import { ImageResponse } from "@vercel/og";
-
 import { Resvg } from "@resvg/resvg-js";
 import satori from "satori";
 
-// @todo
-let emoji = {};
+let inter = {
+  name: "Inter",
+  data: await Bun.file("./public/inter-regular.ttf").arrayBuffer(),
+  weight: 400,
+  style: "normal",
+} as const;
 
-async function generateOgImage(id: string) {
+export async function generateImage({
+  title,
+  description,
+  publishDate,
+}: {
+  title: string;
+  description: string;
+  publishDate: string;
+}) {
   let svg = await satori(
     <div
       style={{
         display: "flex",
+        flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
         width: "100%",
         height: "100%",
         padding: "50px 200px",
         fontSize: 40,
-        color: "black",
         background: "white",
         fontFamily: "Inter",
       }}
     >
-      👋 Hello {id}
+      <h1 style={{ color: "black" }}>{title}</h1>
+      <p style={{ color: "#3b82f6" }}>{description}</p>
+      <p style={{ color: "#3b82f6" }}>{publishDate}</p>
     </div>,
     {
       width: 1200,
       height: 630,
-      embedFont: false,
-      fonts: [
-        {
-          name: "Inter",
-          data: await Bun.file("./public/inter-regular.ttf").arrayBuffer(),
-          weight: 400,
-          style: "normal",
-        },
-      ],
-      graphemeImages: emoji,
+      fonts: [inter],
+      async loadAdditionalAsset(code: string, segment: string) {
+        if (code === "emoji") {
+          // if segment is an emoji
+          let svgText = await Bun.file(
+            `./assets/svg/${segment.codePointAt(0)?.toString(16)}.svg`,
+          ).text();
+          let svgBuf = Buffer.from(svgText).toString("base64");
+          return `data:image/svg+xml;base64,${svgBuf}`;
+        }
+
+        return `missing-segment - ${code} - ${segment}`;
+      },
     },
   );
 
@@ -45,23 +60,17 @@ async function generateOgImage(id: string) {
   let pngData = resvg.render();
   let pngBuffer = pngData.asPng();
 
-  return new Response(pngBuffer, {
-    headers: {
-      "Content-Type": "image/png",
-    },
-  });
+  return pngBuffer;
 }
 
 console.log("Generating og image for 123");
-let res = generateOgImage("123");
-// console.log(res);
-// console.log("Writing to ./public/og-image-123.png");
-// await Bun.write("./public/og-image-123.png", res.body);
-// console.log("Done");
-
-Bun.serve({
-  port: 42069,
-  async fetch() {
-    return await generateOgImage("123");
-  },
+let pngBuffer = await generateImage({
+  title: "Hello",
+  description: "World",
+  publishDate: new Date().toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  }),
 });
+await Bun.write("./public/og-image-test.png", pngBuffer);
