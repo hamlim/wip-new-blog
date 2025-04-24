3a5616c3219462ec736ca3ce4d54e536b0e6ccb1
Matt Hamlin <matthewjameshamlin@gmail.com>
2025-04-24 16:01:49 -0400

    Image gen via pikitia

--- scripts/og-image-gen.tsx
+++ scripts/og-image-gen.tsx
@@ -1,13 +1,15 @@
 /// <reference types="bun" />
-import { Resvg } from "@resvg/resvg-js";
-import satori from "satori";
+// import { Resvg } from "@resvg/resvg-js";
+// import satori from "satori";
 
-let inter = {
-  name: "Inter",
-  data: await Bun.file("./public/inter-regular.ttf").arrayBuffer(),
-  weight: 400,
-  style: "normal",
-} as const;
+// let inter = {
+//   name: "Inter",
+//   data: await Bun.file("./public/inter-regular.ttf").arrayBuffer(),
+//   weight: 400,
+//   style: "normal",
+// } as const;
+
+import { generateImage as generateBaseImage } from "pikitia";
 
 export async function generateImage({
   title,
@@ -18,7 +20,7 @@ export async function generateImage({
   description: string;
   publishDate: string;
 }) {
-  let svg = await satori(
+  let pngBuffer = await generateBaseImage(
     <div
       style={{
         display: "flex",
@@ -35,37 +37,39 @@ export async function generateImage({
     >
       <h1 style={{ color: "black" }}>{title}</h1>
       <p style={{ color: "#3b82f6" }}>{description}</p>
-      <p style={{ color: "#3b82f6" }}>{publishDate}</p>
+      <p style={{ color: "#3b82f6", textDecoration: "underline" }}>
+        {publishDate}
+      </p>
     </div>,
     {
       width: 1200,
       height: 630,
-      fonts: [inter],
-      async loadAdditionalAsset(code: string, segment: string) {
-        if (code === "emoji") {
-          // if segment is an emoji
-          let svgText = await Bun.file(
-            `./assets/svg/${segment.codePointAt(0)?.toString(16)}.svg`,
-          ).text();
-          let svgBuf = Buffer.from(svgText).toString("base64");
-          return `data:image/svg+xml;base64,${svgBuf}`;
-        }
+      // fonts: [inter],
+      // async loadAdditionalAsset(code: string, segment: string) {
+      //   if (code === "emoji") {
+      //     // if segment is an emoji
+      //     let svgText = await Bun.file(
+      //       `./assets/svg/${segment.codePointAt(0)?.toString(16)}.svg`,
+      //     ).text();
+      //     let svgBuf = Buffer.from(svgText).toString("base64");
+      //     return `data:image/svg+xml;base64,${svgBuf}`;
+      //   }
 
-        return `missing-segment - ${code} - ${segment}`;
-      },
+      //   return `missing-segment - ${code} - ${segment}`;
+      // },
     },
   );
 
-  let resvg = new Resvg(svg, {});
-  let pngData = resvg.render();
-  let pngBuffer = pngData.asPng();
+  // let resvg = new Resvg(svg, {});
+  // let pngData = resvg.render();
+  // let pngBuffer = pngData.asPng();
 
   return pngBuffer;
 }
 
 console.log("Generating og image for 123");
 let pngBuffer = await generateImage({
-  title: "Hello",
+  title: "Hello 👋",
   description: "World",
   publishDate: new Date().toLocaleDateString("en-US", {
     month: "long",
