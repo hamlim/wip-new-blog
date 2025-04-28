e52e8700c423175e3ea457490aae78c7645bf427
Matt Hamlin <matthewjameshamlin@gmail.com>
2025-04-28 08:02:54 -0400

    Route gen too

--- src/pages.gen.ts
+++ src/pages.gen.ts
@@ -10,10 +10,20 @@ import type { getConfig as Mdx_getConfig } from './pages/mdx';
 
 // prettier-ignore
 type Page =
+| { path: '/[year]/[month]/[slug]'; render: 'dynamic' }
+| { path: '/[year]/[month]'; render: 'dynamic' }
+| { path: '/[year]'; render: 'dynamic' }
 | { path: '/__sandbox'; render: 'dynamic' }
 | { path: '/_root'; render: 'dynamic' }
+| { path: '/blog'; render: 'dynamic' }
+| { path: '/bookshelf'; render: 'dynamic' }
 | ({ path: '/' } & GetConfigResponse<typeof Index_getConfig>)
-| ({ path: '/mdx' } & GetConfigResponse<typeof Mdx_getConfig>);
+| ({ path: '/mdx' } & GetConfigResponse<typeof Mdx_getConfig>)
+| { path: '/notes'; render: 'dynamic' }
+| { path: '/projects'; render: 'dynamic' }
+| { path: '/resume'; render: 'dynamic' }
+| { path: '/social'; render: 'dynamic' }
+| { path: '/tools'; render: 'dynamic' };
 
 // prettier-ignore
 declare module 'waku/router' {
