86f55916edb23f2c6f23e57eaaf8296ad714d390
Matt Hamlin <matthewjameshamlin@gmail.com>
2025-05-17 13:27:06 -0400

    Cleanup API route

--- src/pages/api/greet.ts
+++
@@ -1,9 +0,0 @@
-import { getHonoContext } from "#utils/get-hono-context";
-
-export default async function handler(_request: Request) {
-  let ctx = getHonoContext();
-
-  // if ctx is null, we're in the build phase of the app
-  console.log(ctx?.req.header("User-Agent"));
-  return new Response("Hello, world!");
-}
