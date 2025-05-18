034e35ed47772ff19cdd220772d15731ea503451
Matt Hamlin <matthewjameshamlin@gmail.com>
2025-05-18 07:36:47 -0400

    Add ogImage field to frontmatter on blog posts

--- scripts/migrate-add-og-image.ts
+++ scripts/migrate-add-og-image.ts
@@ -1,17 +1,12 @@
-import matter from "gray-matter";
 // collect all mdx files, add `ogImage` field to the bottom of their frontmatter
 // `ogImage` should be a string that looks like `/og-images/<slug>.png`
 // where `<slug>` is the slug field in frontmatter
+import matter from "gray-matter";
 import { getMDXFiles } from "./collect-metadata";
 
 let files = await getMDXFiles();
 
-let once = false;
 for (let file of files) {
-  if (once) {
-    process.exit(0);
-  }
-  once = true;
   let content = await Bun.file(file).text();
   let { data, content: markdownContent } = matter(content);
 
@@ -23,7 +18,6 @@ for (let file of files) {
 
   // Write back to file
   let newContent = matter.stringify(markdownContent, data);
-  console.log(newContent);
-  //   await Bun.write(file, newContent);
+  await Bun.write(file, newContent);
   console.log(`Added ogImage to ${file}`);
 }
