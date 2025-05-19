4c3ef245b51618763e02824cec5320d62a38d1c0
Matt Hamlin <matthewjameshamlin@gmail.com>
2025-05-19 18:20:20 -0400

    Add about page actually this time

---
+++ src/pages/about.tsx
@@ -0,0 +1,18 @@
+import { ProseContainer } from "#/components/container";
+import { Heading } from "#/components/heading";
+
+export default function About() {
+  return (
+    <main className="pt-10">
+      <ProseContainer>
+        <Heading level={2}>About</Heading>
+      </ProseContainer>
+    </main>
+  );
+}
+
+export function getConfig() {
+  return {
+    render: "dynamic",
+  };
+}
