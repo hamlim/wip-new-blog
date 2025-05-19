93c32f5f4c8935ada197a5b3973b1dd8110d5d1a
Matt Hamlin <matthewjameshamlin@gmail.com>
2025-05-19 07:41:49 -0400

    Wrap in error boundary

--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -1,11 +1,12 @@
 import type { ReactNode } from "react";
+import { ErrorBoundary } from "#/components/error-boundary";
 import { Nav } from "#/components/nav";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <div className="relative mb-26 min-h-screen">
       <Nav />
-      {children}
+      <ErrorBoundary>{children}</ErrorBoundary>
     </div>
   );
 }
