1f56baed9472a98580cbf417dddf21a656895906
Matt Hamlin <matthewjameshamlin@gmail.com>
2025-05-20 18:50:03 -0400

    Add initial logic for og image gen

--- scripts/build/generate-og-images.tsx
+++ scripts/build/generate-og-images.tsx
@@ -1,5 +1,7 @@
+import matter from "gray-matter";
 /// <reference types="bun" />
 import { generateImage as generateBaseImage } from "pikitia";
+import { collectMetadata, getMDXFiles } from "./collect-metadata";
 
 export async function generateImage({
   title,
@@ -40,14 +42,22 @@ export async function generateImage({
   return pngBuffer;
 }
 
-console.log("Generating og image for 123");
-let pngBuffer = await generateImage({
-  title: "Hello 👋",
-  description: "World",
-  publishDate: new Date().toLocaleDateString("en-US", {
-    month: "long",
-    day: "numeric",
-    year: "numeric",
-  }),
-});
-await Bun.write("./public/og-image-test.png", pngBuffer);
+export async function generateOGImages() {
+  let files = await getMDXFiles();
+
+  let metadata = await collectMetadata(files);
+
+  for (let meta of Object.values(metadata)) {
+    let pngBuffer = await generateImage({
+      title: meta.title,
+      description: meta.description || "",
+      publishDate: new Date(meta.date).toLocaleDateString("en-US", {
+        month: "long",
+        day: "numeric",
+        year: "numeric",
+      }),
+    });
+
+    await Bun.write(`./public/og-images/${meta.slug}.png`, pngBuffer);
+  }
+}
