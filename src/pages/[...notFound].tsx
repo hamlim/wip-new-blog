6b6ff287beb48515eb1df224b1c5c928713143d3
Matt Hamlin <matthewjameshamlin@gmail.com>
2025-05-21 15:29:52 -0400

    Add not found handler

---
+++ src/pages/[...notFound].tsx
@@ -0,0 +1,22 @@
+import { ProseContainer } from "#/components/container";
+import { Heading } from "#/components/heading";
+
+export default function NotFound({ notFound }: { notFound: Array<string> }) {
+  return (
+    <main className="pt-10">
+      <title>404</title>
+      <meta rel="description" content="Page not found" />
+      <ProseContainer>
+        <Heading level={1}>404</Heading>
+        <p>Page not found</p>
+        <code>@TODO: Add a better 404 page 😅</code>
+      </ProseContainer>
+    </main>
+  );
+}
+
+export function getConfig() {
+  return {
+    render: "dynamic",
+  };
+}
