64f02e0c9fa883a4c3beb5b69eaafc5479599629
Matt Hamlin <matthewjameshamlin@gmail.com>
2025-05-21 14:50:15 -0400

    Good enough /about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,3 +1,6 @@
+import { FileUser, GithubIcon } from "lucide-react";
+import { Anchor, LinkAnchor } from "#/components/anchor";
+import { BlueskyIcon } from "#/components/bluesky-icon";
 import { ProseContainer } from "#/components/container";
 import { Heading } from "#/components/heading";
 
@@ -7,7 +10,68 @@ export default function About() {
       <title>About Matt</title>
       <meta rel="description" content="About Matt Hamlin" />
       <ProseContainer>
-        <Heading level={2}>About</Heading>
+        <Heading level={2}>About Matt</Heading>
+        <p className="text-lg">
+          Hey there, I'm Matt. I currently live in Boston and work as a software
+          engineer at HubSpot. In my free time I like to work on several
+          different <LinkAnchor href="/projects">projects</LinkAnchor>, and
+          somehow find time to write some{" "}
+          <LinkAnchor href="/blog">blog posts</LinkAnchor> as well.
+        </p>
+        <Heading level={3}>Socials</Heading>
+        <p>
+          <Anchor href="https://bsky.app/profile/matthamlin.me">
+            <BlueskyIcon className="mr-2 inline-block h-4 w-4" />
+            Find me on Bluesky
+          </Anchor>
+          <br />
+          <Anchor href="https://github.com/mhamlin">
+            <GithubIcon className="mr-2 inline-block h-4 w-4" />
+            Find me on GitHub
+          </Anchor>
+          {/* @TODO: add back resume route when updated */}
+          {/* <br />
+          <LinkAnchor href="/resume">
+            <FileUser className="mr-2 inline-block h-4 w-4" />
+            Check out my resume
+          </LinkAnchor> */}
+        </p>
+        <Heading level={3}>Uses</Heading>
+        <p>
+          I use a few core pieces of hardware and software daily, and I don't
+          usually update these all that often:
+        </p>
+        <Heading level={4}>Hardware</Heading>
+        <ul>
+          <li>
+            <span className="font-bold">Laptop</span> - MacBook Air M2 13"
+            (early 2022)
+          </li>
+          <li>
+            <span className="font-bold">Phone</span> - iPhone 16 Pro Max
+          </li>
+          <li>
+            <span className="font-bold">Keyboard</span> - Nuphy Air75 v2
+          </li>
+          <li>
+            <span className="font-bold">E-reader</span> - Kindle Paperwhite
+          </li>
+        </ul>
+        <Heading level={4}>Software</Heading>
+        <ul>
+          <li>
+            <span className="font-bold">Note taking / Tasks</span> - Obsidian
+          </li>
+          <li>
+            <span className="font-bold">Code Editor</span> - Cursor
+          </li>
+          <li>
+            <span className="font-bold">Terminal</span> - Warp
+          </li>
+          <li>
+            <span className="font-bold">Browser</span> - Arc
+          </li>
+        </ul>
       </ProseContainer>
     </main>
   );
