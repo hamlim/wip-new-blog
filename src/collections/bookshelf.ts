77acd0f3bfd0a69f542f6bfd800f6e57971f187d
Matt Hamlin <matthewjameshamlin@gmail.com>
2025-05-25 20:37:19 -0400

    Reverse bookshelf

--- src/collections/bookshelf.ts
+++ src/collections/bookshelf.ts
@@ -1,5 +1,11 @@
 import bookshelf from "#/bookshelf.json";
 
-export let reading = bookshelf.filter((book) => book.status === "reading");
-export let toRead = bookshelf.filter((book) => book.status === "to-read");
-export let read = bookshelf.filter((book) => book.status === "read");
+export let reading = bookshelf
+  .filter((book) => book.status === "reading")
+  .toReversed();
+export let toRead = bookshelf
+  .filter((book) => book.status === "to-read")
+  .toReversed();
+export let read = bookshelf
+  .filter((book) => book.status === "read")
+  .toReversed();
